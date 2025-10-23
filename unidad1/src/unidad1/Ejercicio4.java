aa6bdb0e0bebc5ee6d5bee8677184b570e73746d
Samueljimenez444 <samuel.jimenez@iesnervion.es>
2025-10-23 10:18:36 +0200

    Ejercicios Completados

---
+++ unidad1/src/unidad1/Ejercicio4.java
@@ -0,0 +1,46 @@
+package unidad1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		String rutaFichero = "C:\\Users\\samuel.jimenez\\palabras.txt";
+
+		String rutaFicheroNuevo = "C:\\Users\\samuel.jimenez\\palabrasNuevas2.txt";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(rutaFichero))) {
+
+			String linea;
+
+			while ((linea = br.readLine()) != null) {
+
+				String[] palabras = linea.split("(?=[A-Z])");
+				
+				Arrays.sort(palabras);
+
+				try (BufferedWriter bw = new BufferedWriter(new FileWriter(rutaFicheroNuevo))) {
+
+					for (String palabra : palabras) {
+
+						bw.write(palabra);
+
+						bw.newLine();
+					}
+				} catch (IOException e) {
+
+				}
+				System.out.println("TXT Formado Correctamente");
+			}
+		} catch (IOException e) {
+
+		}
+
+	}
+
+}
